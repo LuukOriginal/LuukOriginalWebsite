02b910619b1101497ecfdd8276aabcad3b354233
LuukOriginal <LuukOriginal@gmail.com>
2022-03-06 15:33:16 +0100

    first version

---
+++ README.md
@@ -0,0 +1,2 @@
+# LuukOriginal Website #
+Hello World!
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {} from 'react-bootstrap'
+
+export default function Home() {
+  return (
+    <div id='Home'>
+      <div class="mt-4 p-5 bg-primary text-white rounded">
+        <h1>Under Constuction</h1>
+        <p>Come back later...</p>
+      </div>
+    </div>
+  )
+}
