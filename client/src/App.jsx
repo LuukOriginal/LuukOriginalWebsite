2c3123da6fb95911cdda2afcc2d46a10ee8939fd
LuukOriginal <LuukOriginal@gmail.com>
2022-05-16 08:33:45 +0200

    push to src branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <MainNav/>
       <Routes>
         <Route path='/' element={<Home/>} />
-        {/* <Route path='about' element={<About/>} /> */}
         <Route path='*' element={<NotFound/>} />
       </Routes>
     </div>
